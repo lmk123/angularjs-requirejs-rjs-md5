e02e595763278283146ced723e9f7c9169f767d9
Milk Lee <milk.lee@qq.com>
2015-04-07 14:06:29 +0800

    修复 ui-router 的一个 bug

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -24,9 +24,9 @@ define( [
     angular.module( 'app' , [ 'ui.router' , 'controllers' ] )
         .config( [
             '$stateProvider' ,
-            '$urlRouterProvider' ,
-            function ( $stateProvider , $urlRouterProvider ) {
-                $urlRouterProvider.otherwise( '/index' );
+            //'$urlRouterProvider' ,
+            function ( $stateProvider /*, $urlRouterProvider*/ ) {
+                //$urlRouterProvider.otherwise( '/index' );
 
                 // 设置路由
                 $stateProvider.state( 'login' , {
@@ -57,6 +57,21 @@ define( [
                             ] )
                         }
                     } );
+
+                // 不能使用下面这句代码：
+                // $urlRouterProvider.otherwise( '/index' );
+                // 见 http://stackoverflow.com/questions/25065699/why-does-angularjs-with-ui-router-keep-firing-the-statechangestart-event
+                // 另外，这段代码必须放在最后一个路由，否则直接在链接中到 #/路由 会无效
+                $stateProvider.state( 'otherwise' , {
+                    url : '*path' ,
+                    template : '' ,
+                    controller : [
+                        '$state' ,
+                        function ( $state ) {
+                            $state.go( 'logged.index' );
+                        }
+                    ]
+                } );
 
                 /**
                  * 加载依赖的辅助函数
