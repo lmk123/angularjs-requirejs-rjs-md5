af43c2ae0d75c8f317db5fd63c181dd7821102e1
Milk Lee <milk.lee@qq.com>
2015-04-21 13:13:35 +0800

    在测试用例中修复加载模板时可能会出现的测试失败情况

--- test/e2e/loginSpec.js
+++ test/e2e/loginSpec.js
@@ -1,10 +1,10 @@
 var appUrl = 'http://localhost:61111/demo-angularjs-requirejs/app/';
 describe( '测试应用' , function () {
-
-    // 测试偶尔会失败，原因可能是当测试执行到 $('button') 的时候，
-    // 页面还没来得及跳转到 #login ，因为模板文件是异步加载的
-   it( '随便进入一个地址时应该跳转到登录页面' , function () {
+    it( '随便进入一个地址时应该跳转到登录页面' , function ( done ) {
         browser.get( appUrl + '#/some-url' );
-        expect( $( 'button' ).getText() ).toEqual( '登录' );
+        browser.sleep( 100 ).then( function () { // 等一会儿，让模板加载完，否则测试偶尔会失败
+            expect( $( 'button' ).isPresent() ).toEqual( true );
+            done();
+        } );
     } );
 } );
