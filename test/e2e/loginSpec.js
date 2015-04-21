cf64b57c4268e5c393f74e6a6526b769aff64b6d
Milk Lee <milk.lee@qq.com>
2015-04-21 11:45:50 +0800

    优化端到端测试

--- test/e2e/loginSpec.js
+++ test/e2e/loginSpec.js
@@ -1,21 +1,9 @@
 var appUrl = 'http://localhost:61111/demo-angularjs-requirejs/app/';
 describe( '测试应用' , function () {
-    beforeAll( function () {
 
-        // 将原本的 bootstrap 覆盖掉，其实就是加了一个 ngMockE2E 模块
-        browser.addMockModule( 'bootstrap' , function () {
-            angular
-                .module( 'bootstrap' , [ 'ngMockE2E' , 'ui.router' , 'app' ] )
-                .run( [
-                    '$httpBackend' ,
-                    function ( $httpBackend ) {
-                        $httpBackend.whenGET( /.*/ ).passThrough();
-                        $httpBackend.whenPOST( /.*/ ).passThrough();
-                    }
-                ] );
-        } );
-    } );
-    it( '随便进入一个地址时应该跳转到登录页面' , function () {
+    // 测试偶尔会失败，原因可能是当测试执行到 $('button') 的时候，
+    // 页面还没来得及跳转到 #login ，因为模板文件是异步加载的
+   it( '随便进入一个地址时应该跳转到登录页面' , function () {
         browser.get( appUrl + '#/some-url' );
         expect( $( 'button' ).getText() ).toEqual( '登录' );
     } );
--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,7 +1,25 @@
 // conf.js
 exports.config = {
     framework : 'jasmine2' ,
-    seleniumAddress : 'http://localhost:4444/wd/hub' ,
-    specs : [ './e2e/**/*Spec.js' ]
+    directConnect : true , // 直接通过 chrome driven 开始测试，这样不需要启动 selenium 服务器，比较方便
+    //seleniumAddress : 'http://localhost:4444/wd/hub' ,
+    specs : [ './e2e/**/*Spec.js' ] ,
+
+    // 将 $httpBackend 放在 onPrepare 里
+    onPrepare : function () {
+
+        // 将原本的 bootstrap 覆盖掉，其实就是加了一个 ngMockE2E 模块
+        browser.addMockModule( 'bootstrap' , function () {
+            angular
+                .module( 'bootstrap' , [ 'ngMockE2E' , 'ui.router' , 'app' ] )
+                .run( [
+                    '$httpBackend' ,
+                    function ( $httpBackend ) {
+                        $httpBackend.whenGET( /.*/ ).passThrough();
+                        $httpBackend.whenPOST( /.*/ ).passThrough();
+                    }
+                ] );
+        } );
+    }
 };
 
