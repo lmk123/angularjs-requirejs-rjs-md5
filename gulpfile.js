53a3db7a8393f5b274a9a0cea93b3c917f96467b
Milk Lee <milk.lee@qq.com>
2015-06-19 16:07:42 +0800

    改进模板文件的 cdn 前缀替换算法

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
-var SRC           = 'app' ,
-    REQUIREJS     = 'build-requirejs' ,
-    DIST          = 'build' ,
-    CDN           = 'cdn' ,
+var SRC              = 'app' ,
+    REQUIREJS        = 'build-requirejs' ,
+    DIST             = 'build' ,
+    CDN              = 'cdn' ,
 
     // 如果不是 null，那么这个值会作为 cdn 前缀追加到需要加载的文件里。
     // 这里作为示例使用了 https 的链接
-    CDN_PREFIX    = 'https://lmk123.github.io/angularjs-requirejs-rjs-md5/cdn/' ,
-    paths         = {
+    CDN_PREFIX       = 'https://lmk123.github.io/angularjs-requirejs-rjs-md5/cdn/' ,
+    paths            = {
         js : [
             REQUIREJS + '/**/*.js'
         ] ,
@@ -16,37 +16,48 @@ var SRC           = 'app' ,
         copyFiles : [ REQUIREJS + '/**/*' , '!' + REQUIREJS + '/**/*.{js,css,html}' , '!' + REQUIREJS + '/build.txt' ]
     } ,
 
-    fs            = require( 'fs' ) ,
-    gulp          = require( 'gulp' ) ,
-    minifyJS      = require( 'gulp-uglify' ) ,
-    minifyCSS     = require( 'gulp-minify-css' ) ,
-    minifyHTML    = require( 'gulp-htmlmin' ) ,
+    fs               = require( 'fs' ) ,
+    gulp             = require( 'gulp' ) ,
+    minifyJS         = require( 'gulp-uglify' ) ,
+    minifyCSS        = require( 'gulp-minify-css' ) ,
+    minifyHTML       = require( 'gulp-htmlmin' ) ,
 
     //changed    = require( 'gulp-changed' ) ,
-    concat        = require( 'gulp-concat' ) ,
-    deleteFile    = require( 'del' ) ,
-
-    revAllOptions = {
+    concat           = require( 'gulp-concat' ) ,
+    deleteFile       = require( 'del' ) ,
+    indexHtmlContent = fs.readFileSync( SRC + '/index.html' , { encoding : 'utf8' } ) ,
+    revall           = new (require( 'gulp-rev-all' ))( {
         dontRenameFile : [ /^\/index\.html$/ ] ,
         transformFilename : function ( file , hash ) {
             return hash + file.path.slice( file.path.lastIndexOf( '.' ) );
+        } ,
+        transformPath : function ( rev , source , file ) {
+            //console.log( rev , source );
+            // 如果这个文件在 src/index.html 里出现了，或者是 html 文件（即模板文件），就加上 cdn 前缀
+            if ( (CDN_PREFIX && indexHtmlContent.search( '"' + source + '"' ) > 0) || '.html' === source.slice( -5 ) ) {
+                return CDN_PREFIX + rev;
+            }
+            return rev;
         }
-    };
+    } );
 
-if ( CDN_PREFIX ) {
-    var indexHtmlContent = fs.readFileSync( SRC + '/index.html' , { encoding : 'utf8' } );
+gulp.task( 'clean' , clean );
 
-    revAllOptions.transformPath = function ( rev , source , file ) {
-        //console.log( rev , source );
-        // 如果这个文件在 src/index.html 里出现了，就加上 cdn 前缀
-        if ( indexHtmlContent.search( '"' + source + '"' ) > 0 ) {
-            return CDN_PREFIX + rev;
-        }
-        return rev;
-    };
-}
+gulp.task( 'requirejs' , requirejs ); //第一步： 从 SRC 把文件合并至 REQUIREJS 文件夹
 
-var revall = new (require( 'gulp-rev-all' ))( revAllOptions );
+gulp.task( 'prefix' , [ 'requirejs' ] , prefix ); // 第二步：给 REQUIREJS 文件夹下的文件添加 cdn 前缀
+
+// 第三步：下面四个操作是并行的，用于将 REQUIREJS 文件夹下的文件精简至 DIST 文件夹
+gulp.task( 'js' , [ 'prefix' ] , js );
+
+gulp.task( 'css' , [ 'prefix' ] , css );
+
+gulp.task( 'html' , [ 'prefix' ] , html );
+
+gulp.task( 'copy' , [ 'prefix' ] , copy );
+
+// 第四步：将 DIST 文件夹下的文件打上 md5 签名并输出到 CDN 文件夹
+gulp.task( 'default' , [ 'js' , 'css' , 'html' , 'copy' ] , md5 );
 
 function prefix( done ) {
     if ( CDN_PREFIX ) {
@@ -56,7 +67,6 @@ function prefix( done ) {
         fs.writeFileSync(
             bootStrapFilePath ,
             fs.readFileSync( bootStrapFilePath , { encoding : 'utf8' } )
-                .replace( /var\s+_CDN_PREFIX_\s*=\s*('|")\.\/\1/ , 'var _CDN_PREFIX_ ="' + CDN_PREFIX + '"' )
                 .replace( /baseUrl\s*:\s*('|")\.\/\1/ , 'baseUrl:"' + CDN_PREFIX + '"' ) // require.config 里面的 baseUrl 不能直接写成上面的变量，所以得单独替换
         );
     }
@@ -124,21 +134,3 @@ function requirejs( done ) {
         done();
     } );
 }
-
-gulp.task( 'clean' , clean );
-
-gulp.task( 'requirejs' , requirejs ); //第一步： 从 SRC 把文件合并至 REQUIREJS 文件夹
-
-gulp.task( 'prefix' , [ 'requirejs' ] , prefix ); // 第二步：给 REQUIREJS 文件夹下的文件添加 cdn 前缀
-
-// 第三步：下面四个操作是并行的，用于将 REQUIREJS 文件夹下的文件精简至 DIST 文件夹
-gulp.task( 'js' , [ 'prefix' ] , js );
-
-gulp.task( 'css' , [ 'prefix' ] , css );
-
-gulp.task( 'html' , [ 'prefix' ] , html );
-
-gulp.task( 'copy' , [ 'prefix' ] , copy );
-
-// 第四步：将 DIST 文件夹下的文件打上 md5 签名并输出到 CDN 文件夹
-gulp.task( 'default' , [ 'js' , 'css' , 'html' , 'copy' ] , md5 );
